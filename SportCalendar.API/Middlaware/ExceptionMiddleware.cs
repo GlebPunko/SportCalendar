9902bd1f83edd1fc34eb2ea942d1f377d11688ec
Hleb Punko <hleb.punko01@gmail.com>
2024-11-17 21:59:10 +0300

    Add exception and context middleware

---
+++ SportCalendar.API/Middlaware/ExceptionMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using SportCalendar.API.Helpers.Response;
+using SportCalendar.API.Helpers;
+using SportCalendar.Domain.CustomExceptions;
+using SportCalendar.Domain.Enums;
+using System.Net;
+using SportCalendar.API.Attributes;
+
+namespace SportCalendar.API.Middlaware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestContext requestContext)
+        {
+            try
+            {
+                await _next(context);
+
+                var hasNoReturns = requestContext.Endpoint?.Metadata.GetMetadata<NoReturnItemsAttribute>() is not null;
+
+                if (!hasNoReturns && requestContext?.Endpoint?.Metadata.Any() == true)
+                {
+                    if (!context.Response.HasStarted)
+                    {
+                        var data = context.Items["data"];
+
+                        var response = new ResponseSuccess(data);
+
+                        context.Response.StatusCode = (int)HttpStatusCode.OK;
+                        await context.Response.WriteAsJsonAsync(response);
+                    }
+                }
+            }
+            catch (ResponseBaseException ex)
+            {
+                var data = ex.ErrorData ?? context.Items["data"];
+
+                object error = null;
+#if DEBUG
+                error = $"Error: {ex.Message}\n\n{ex.StackTrace}";
+#endif
+
+                var response = new ResponseError(ex.ResponseStatus, data, error);
+
+                context.Response.StatusCode = (int)ex.StatusCode;
+                await context.Response.WriteAsJsonAsync(response);
+            }
+            catch (Exception ex)
+            {
+                object error = null;
+
+#if DEBUG
+                error = $"Error: {ex.Message}\n\n{ex.StackTrace}";
+#endif
+                //TODO Logger for log ex
+
+                var response = new ResponseError(ResponseStatus.Error_Unknown, null, error);
+
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await context.Response.WriteAsJsonAsync(response);
+            }
+        }
+    }
+}
--- SportCalendar.API/Program.cs
+++ SportCalendar.API/Program.cs
@@ -1,17 +1,39 @@
+using SportCalendar.API.Helpers;
+using SportCalendar.API.Middlaware;
 using SportCalendar.Application.DI;
 using SportCalendar.DataAccess.DI;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataAccess(builder.Configuration);
 builder.Services.AddApplication();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(conf =>
+{
+    conf.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+});
+
+builder.Services.AddScoped<RequestContext>();
+
+builder.Services.AddCors(options => 
+    options.AddDefaultPolicy(
+        builder => builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .SetIsOriginAllowed(origin => true)
+            .AllowCredentials()
+    ));
 
 var app = builder.Build();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseMiddleware<ContextMiddleware>();
+
+app.UseCors();
 
 app.Run();
